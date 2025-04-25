aad4e359cb56993312ef4d8dfc6875b356339d9c
Rafael Patrick de Souza <faelpatrick@gmail.com>
2025-04-25 22:40:14 +0100

    Adicionar projeto "Pokedex" para o aluno Lucas na página de alunos

--- pages/alunos/[aluno].vue
+++ pages/alunos/[aluno].vue
@@ -69,6 +69,9 @@ const projetosPorAluno = {
 	'lisa': [
 		{ titulo: 'Jogo dos Presentes no Scratch', link: 'https://scratch.mit.edu/projects/1147599174' },
 	],
+	'lucas': [
+		{ titulo: 'Pokedex', link: 'https://pok-dex-gray.vercel.app' },
+	],
 	'tomas': [
 		{ titulo: 'Jogo dos Presentes no Scratch', link: 'https://scratch.mit.edu/projects/1147600234' },
 	],
